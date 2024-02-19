7153e278735f705886c1b7fe4632871aeab4ea6e
tae_98 <k99779004@naver.com>
2024-02-19 16:14:56 +0900

    Update final.ino

--- ard/final.ino
+++ ard/final.ino
@@ -7,6 +7,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(7,OUTPUT);
   pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -18,17 +19,25 @@ void loop() {
     int degree;
     degree = input.toInt();
     Serial.println(degree);
-    if(degree>200){
+    if(degree>260){
       Serial.println('a');
       digitalWrite(7,1);
       digitalWrite(6,0);
+      digitalWrite(5,0);
      
       delay(100);
     }
-    else if(degree<200){
+    else if(degree>130){
       digitalWrite(7,0);
       digitalWrite(6,1);
+      digitalWrite(5,0);
       delay(100);
+    }
+    else if (degree<130){
+      digitalWrite(7,0);
+      digitalWrite(6,0);
+      digitalWrite(5,1);
+      
     }
 
     //input == '0';
